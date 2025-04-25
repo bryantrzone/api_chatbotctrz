e807793fd385860ef40fe010a9fa6a9bd8ea4b01
bryantrzone <bryan.martinez@tractozone.com.mx>
2025-04-25 15:49:14 -0600

    Configuración de webhooks

--- chats.php
+++ chats.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 // Incluir configuración de base de datos
 require_once "db.php";
 
